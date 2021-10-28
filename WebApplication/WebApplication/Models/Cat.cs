9eab203fff5e1fdbc6c63767758ebc5d0ca05010
Ania <annamikulak99@gmail.com>
2021-10-28 16:28:26 +0200

    Models added

---
+++ WebApplication/WebApplication/Models/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Cat
+    {
+        public int IdCat { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
